3aea376db4d0e303f08187966f0009bda05bfad7
Ash McKenzie <ash@greenworm.com.au>
2012-11-26 23:30:38 +1100

    added missing index

--- lib/models/log_entry.rb
+++ lib/models/log_entry.rb
@@ -5,6 +5,7 @@ class LogEntry
   field :raw, type: String
 
   index({ type: 1 }, { background: true })
+  index({ timestamp: -1 }, { background: true })
 
   def source_type
     self.class.to_s.underscore
